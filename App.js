398fdc6322aeefab33c292a9791f8c150ca71184
echugreeva <e.chugreeva@gmail.com>
2023-12-09 18:50:50 +0200

    fixed dependencies

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default function App() {
 
   return (
     <NavigationContainer >
+      {/* <Signup/> */}
       <Stack.Navigator screenOptions={{
         headerStyle: {
           backgroundColor: '#009387',
